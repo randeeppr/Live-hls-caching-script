d8fada5b8c0ae0b1b038757110b37b9cd192c093
Randeep <randeep123@gmail.com>
2016-08-25 00:14:05 +0530

    Added hls_from_file.sh
    This file downloads the hls manifest file, parse it, and downloads all the chunks specified in the best profile sub manifest file.

---
+++ hls_from_file.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+#The script download the manifest file of the given hls stream, parse it and downloads the ts chunks from the stream. logs the details to hls.log in the current directory.
+#Revision 1.0
+#author : Randeep
+#Contact : randeep123@gmail.com
+
+#this script will be invoked by the following script.
+##!/bin/bash
+#for i in `cat hls_inputs.txt`
+#do
+#/bin/sh hls_from_file.sh $i &
+#done
+
+#creating channel directory
+if [ ! -d channels ];then
+        mkdir channels
+fi
+cd channels
+
+url=$1
+channelname=`echo $url | cut -d "/" -f 5`
+if [ ! -d $channelname ];then
+        cd /ssd/hls/channels/
+        mkdir $channelname
+fi
+cd /ssd/hls/channels/$channelname
+
+while true
+do
+        cd /ssd/hls/channels/$channelname
+        #deleting existing .m3u8 and .ts files
+        rm -f *.m3u8* *.ts*
+
+        #Getting the manifest file
+        wget $url -o /dev/null
+
+        #Creating the baseurl so that the chunks can be appendend to it and requested.
+        baseurl=`echo $url | cut -d "/" -f 1,2,3,4,5`
+        #echo -e "Baseurl is : $baseurl" #>> hls_$channelname.log
+
+        #Parseing the name of the highest resolution/bitrate manifest
+        submanifestname=`cat index.m3u8 | grep 1- | tr -d '\r'`
+        suburl=$baseurl/$submanifestname
+        #echo -e "`date` submanifesturl is : $suburl" #>> hls_$channelname.log
+
+        #Downloading the submanifest
+        wget $suburl -o /dev/null
+        for i in `cat $submanifestname | grep 1-`
+                do
+                        #echo -e "`date` $baseurl/`echo $i|tr -d '\r'`" #>> hls_$channelname.log
+                        wget $baseurl/`echo $i|tr -d '\r'` -O /dev/null -o /dev/null
+                done
+
+        #deleting existing .m3u8 and .ts files
+        rm -f *.m3u8* *.ts*
+        sleep 3
+done
