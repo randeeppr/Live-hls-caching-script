71c880c7e18e40a990946647a76cdbc1308fac52
Randeep <randeep123@gmail.com>
2016-08-26 01:56:29 +0530

    Optimizing the script
    wrapped all variables in quotes. Removed unnecessary echos.

--- hls_from_file.sh
+++ hls_from_file.sh
@@ -5,46 +5,42 @@
 #Contact : randeep123@gmail.com
 
 #creating channel directory
-if [ ! -d channels ];then
-        mkdir channels
-fi
-cd channels
+mkdir -p channels && cd channels
 
 url=$1
-channelname=`echo $url | cut -d "/" -f 5`
-if [ ! -d $channelname ];then
-        cd /ssd/hls/channels/
-        mkdir $channelname
-fi
-cd /ssd/hls/channels/$channelname
+channelname=`cut -d "/" -f 5 <<< $url`
+mkdir -p /ssd/hls/channels/"$channelname" && cd /ssd/hls/channels/"$channelname"
+
+cleanup(){
+        rm -f *.m3u8* *.ts*
+}
 
 while true
 do
-        cd /ssd/hls/channels/$channelname
         #deleting existing .m3u8 and .ts files
-        rm -f *.m3u8* *.ts*
+        cleanup
 
         #Getting the manifest file
-        wget $url -o /dev/null
+        wget "$url" -o /dev/null
 
         #Creating the baseurl so that the chunks can be appendend to it and requested.
-        baseurl=`echo $url | cut -d "/" -f 1,2,3,4,5`
+        baseurl=`cut -d "/" -f 1,2,3,4,5 <<< "$url"`
         #echo -e "Baseurl is : $baseurl" #>> hls_$channelname.log
 
         #Parseing the name of the highest resolution/bitrate manifest
         submanifestname=`cat index.m3u8 | grep 1- | tr -d '\r'`
-        suburl=$baseurl/$submanifestname
+        suburl="$baseurl"/"$submanifestname"
         #echo -e "`date` submanifesturl is : $suburl" #>> hls_$channelname.log
 
         #Downloading the submanifest
-        wget $suburl -o /dev/null
-        for i in `cat $submanifestname | grep 1-`
+        wget "$suburl" -o /dev/null
+        for i in `cat "$submanifestname" | grep 1-`
                 do
                         #echo -e "`date` $baseurl/`echo $i|tr -d '\r'`" #>> hls_$channelname.log
-                        wget $baseurl/`echo $i|tr -d '\r'` -O /dev/null -o /dev/null
+                        wget "$baseurl"/`echo $i|tr -d '\r'` -O /dev/null -o /dev/null
                 done
 
         #deleting existing .m3u8 and .ts files
-        rm -f *.m3u8* *.ts*
+        cleanup
         sleep 3
 done
